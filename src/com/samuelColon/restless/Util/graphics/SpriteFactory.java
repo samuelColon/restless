190f94d721c15316fe31b4080f839685ec10037d
samuelColon <samsoncs@gmail.com>
2015-05-21 13:29:08 -0400

    stashin

---
+++ src/com/samuelColon/restless/Util/graphics/SpriteFactory.java
@@ -0,0 +1,28 @@
+package com.samuelColon.restless.Util.graphics;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/** Place holder class
+ * */
+public class SpriteFactory {
+
+    private BufferedImage sprite;
+    public SpriteFactory () {
+    }
+
+    public void initSprite(File file) {
+        sprite = null;
+        try {
+            sprite = ImageIO.read( file );
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public BufferedImage getSprite() {
+        return sprite;
+    }
+}
